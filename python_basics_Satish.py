cb8f3bcea1c39850a2a2362f26ce7e3983465061
Satish-2804 <284satish@gmail.com>
2026-04-16 17:55:58 +0530

    intial commit

---
+++ python_basics_Satish.py
@@ -0,0 +1,10 @@
+name = "Satish"
+age = 40
+height_mtr = 1.74
+is_student = True
+print(name, age, height_mtr, is_student)
+print("Name:", name, "| Age:", age, "| Height:", height_mtr, "| Student:", is_student)
+print(age * 12)
+print(age * 365)
+print(age % 7)
+print(age ** 2)
--- test.py
+++ test.py
@@ -1,1 +1,3 @@
-print("Hello World")
+name = 10
+print(type(name)) # to know the data type,  use keyword 'type'
+print(id(name))
